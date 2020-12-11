629431b9767a188ac8951b2933769d9934fd693f
Omkar Jadhav <omkar.jadhav00@gmail.com>
2020-12-11 18:59:39 +0530

    First commmit

---
+++ Detect-mask.py
@@ -0,0 +1,117 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import cv2
+
+# faceNet captures the face from the frame
+# maskNet checks the if mask /No-mask
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # Take the dimensions of the frame and then construct a blob from it
+    #blob is processed image
+    # cv2.dnn.blobFromImage pre-processes the image. It does the mean subtraction for the RGB values
+    # on the image & scales it with 1
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
+    # Pass the blob through network and obtain face detection
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    # detections are -[~,~,prediciton,startX,startY,endX,endY] for the detected faces in the frame
+
+    # initialize the list of faces, their corresponding locations,
+    # and the list of predictions from our face network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence associated with the detection
+        confidence = detections[0, 0, i, 2]
+
+    # filter out the weak detections by ensuring the confidence is greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x,y) co-ordinates of the bounding boxes for the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # Ensure bounding boxes fall within the diimensions of the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI convert it from BGR to RGB channel
+            # ordering, resizing it to 224*224 and process it
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # Add the face and the bounding boxes to their respective lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a prediction if atleast one face was detected
+    if len(faces) > 0:
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding locations
+    return (locs, preds)
+
+
+# load out face detector model from disk
+prototxtpath = r'face_detector\deploy.prototxt'
+weightspath = r'face_detector\res10_300x300_ssd_iter_140000.caffemodel'
+faceNet = cv2.dnn.readNet(prototxtpath, weightspath)
+
+# load the dace mask detecctor model from the disk
+maskNet = load_model("Face_mask_detector.model")
+
+# Initialize  the video stream
+print("Starting video stream...")
+vs = VideoStream(src=0).start()  # Source =0 gives the primary camera
+
+# Loop over the frames from the video frame
+while True:
+    # Grab the frame from the threaded video stream and resize it to have
+    # a maximum width of 800 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=800)
+
+    # Detect faces in the frame and determine if they are wearing a mask or not
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+    # loop over the detected face location and their corresponding location
+    for (box, pred) in zip(locs, preds):
+        # Unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutmask) = pred
+
+        # Determine the class label and color to draw the bounding boxes and text
+        label = 'Mask' if mask > withoutmask else "No mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probabilit in the label
+        label = "{}:{:.2f}%".format(label, max(mask, withoutmask) * 100)
+
+        # display the label and the bounding box reactangle on the output frame
+        cv2.putText(frame, label,(startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    # Show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the q was pressed , break from the loop
+    if key == ord("q"):
+        break
+
+# do clean up
+cv2.destroyAllWindows()
+vs.stop()
---
+++ Face-mask detection 1.py
@@ -0,0 +1,133 @@
+# Import the packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Initialise the batch size
+batch_size=32
+
+#Getting the data
+directory=r"dataset"
+categories=["with_mask","without_mask"]
+
+#Initializing the list of data and class images
+print("loading images...")
+data=[]     # Data lists
+labels=[]       #Labels list
+
+
+for category in categories:
+    path=os.path.join(directory,category)
+    for img in os.listdir(path):
+        img_path = os.path.join(path,img)
+        image = load_img(img_path,target_size=(224,224))  #from keras.preprocessing
+        image = img_to_array(image)       #Convert image to array
+        image = preprocess_input(image)   #Using resnet50
+
+        data.append(image)
+        labels.append(category)
+
+# Do one hot encoding for the labels. (since initially it were strings)
+lb=LabelBinarizer()
+labels=lb.fit_transform(labels)
+labels=to_categorical(labels)
+
+#Convert lists into numpy arrays
+data=np.array(data,dtype='float32')
+labels=np.array(labels)
+
+#Splitting the data 80% train and 20% test
+# Stratify keeps the same % of labelled data in both test and train
+(trainX,testX,trainY,testY)=train_test_split(data,labels,test_size=0.2,
+                                stratify=labels,random_state=42)
+
+#perform data augmentation
+aug=ImageDataGenerator(
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+#Load the ResNet network, Not including the top layer of the data
+baseModel=MobileNetV2(weights="imagenet",include_top=False,
+                      input_tensor=Input(shape=(224, 224, 3)))
+
+#Construct the head of the model that will be placed on top of the base model
+headmodel = baseModel.output
+headmodel = AveragePooling2D(pool_size=(7, 7))(headmodel)
+headmodel = Flatten(name="flatten")(headmodel)
+headmodel = Dense(128,activation='relu')(headmodel)
+headmodel = Dropout(0.5)(headmodel)
+headmodel = Dense(2,activation='softmax')(headmodel)
+
+
+#Place the head FC model on top of base model
+#Complete model
+model = Model(inputs=baseModel.input, outputs=headmodel)
+
+
+# Freeze the layers in the baseModel
+for layer in baseModel.layers:
+    layer.trainable=False
+
+#Compiling the model
+print(" compiling model...")
+optim=Adam(lr=init_LR,decay=init_LR/epochs)
+model.compile(loss="binary_crossentropy",optimizer='adam',
+              metrics=["accuracy"])
+
+#train the head model
+print(" Training head model...")
+history=model.fit(aug.flow(trainX,trainY,
+             batch_size=batch_size),
+             steps_per_epoch=len(trainX)//batch_size,
+             validation_data=(testX,testY),
+             validation_steps=len(testX)//batch_size,
+             epochs=20,verbose=1)
+
+# Make predictions on the testing set
+print("Evaluating network..")
+predIdxs=model.predict(testX,batch_size=batch_size)
+
+#For each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs=np.argmax(predIdxs,axis=1)
+
+#Shows a nicely forrmatted classification report
+print(classification_report(testY.argmax(axis=1),predIdxs,
+                            target_names=lb.classes_))
+#Serialuze the model to disk
+print("Saving the model...")
+model.save("Face_mask_detector.model",save_format="h5")
+
+#Plot training loss and accuracy
+N=epochs
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arrange(0,N),history.history['loss'],label='train_loss')
+plt.plot(np.arrange(0,N),history.history['val_loss'],label='val_loss')
+plt.plot(np.arrange(0,N),history.history['accuracy'],label='train_acc')
+plt.plot(np.arrange(0,N),history.history['val_accuracy'],label='val_acc')
+plt.title("Training loss and accuracy")
+plt.xlabel('Epochs')
+plt.ylabel('Loss/Accuracy')
